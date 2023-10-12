4b5af00fcc16b3015453ca9c5cadd19c734ebe2a
Vilius Sapagovas <vilius.sapagovas2000@gmail.com>
2023-10-12 16:15:04 +0200

    adding main_loops.py to repo

---
+++ main_loops.py
@@ -0,0 +1,12 @@
+# Create a program witch takes 5 different (minmum)  variables containing strings for username and password. (One input)
+# The string should look like : "username='', password=''; username='',password='';....etc"
+# Start Endless loop allowing to enter username and password and 
+# check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
+# The program should show numbers of times you tried to enter both credentials.
+
+# 0. Susikurti nauja repository github, klonuoti pagrindine repository i nauja, susikurti nauja branch ir pradet daryt dalykus.
+# 1. Sukurti 5 account su vienu input formate "username , password ; "
+# 2. Sukurti cikla ir sukelti viska i dictionary; 
+# 3. parasyti mini prisijungimo programa, imituoti prsijungima i account, patikrint username, password ir jeigu viskas sutampa "ok", jei ne vesti is naujo.
+# 4. Sukurti cikla, patikrinti ar password stiprus, pasiulyti stipru password;
+
